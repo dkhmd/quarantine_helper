ed4b4c9d30444f74a991ad7926b8f10740394382
airkei <keisuke.nakata0114@gmail.com>
2021-10-09 17:53:52 +0900

    remove unnecessary files

--- rasp/gateway/publish.py
+++ rasp/gateway/publish.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 import argparse
 from awscrt import io, mqtt, auth, http
 from awsiot import mqtt_connection_builder
