fb41ec9556130bf4f853f092e1ca7899c9430fc2
dkhmd <hamadakun@hotmail.com>
2021-11-23 16:51:50 +0900

    add timestamp for database column

--- rasp/dummy_gateway.py
+++ rasp/dummy_gateway.py
@@ -5,7 +5,8 @@ import publish
 import random
 import ibscanner
 
-devaddr = "01:23:45:67:89:AB"
+devaddr = "dc:a6:32:71:35:46"
+devuuid = '00000000-123c-1001-B000-001c4D05C3A8'
 ibinfo = {}
 
 def prettify(mac_string):
@@ -19,10 +20,11 @@ def create_dict_data():
     now = dt.now()
     dict_data = {}
     dict_data['date'] = now.strftime('%Y-%m-%d %H:%M:%S')
+    dict_data['timestamp'] = now.timestamp()
     dict_data['address'] = devaddr
 
     dict_data['number_of_beacon'] = len(ibinfo)
-    dict_data['beaon'] = []
+    dict_data['beacon'] = []
     for k, v in ibinfo.items():
         binfo = {}
         binfo['address'] = k
@@ -30,7 +32,7 @@ def create_dict_data():
         binfo['major'] = v[1]['major']
         binfo['minor'] = v[1]['minor']
         binfo['uuid'] = v[1]['uuid']
-        dict_data['beaon'].append(binfo)
+        dict_data['beacon'].append(binfo)
 
     dict_data['action'] = getRaondomAction()
 
@@ -44,7 +46,7 @@ if __name__ == "__main__":
     parser.add_argument('-k', '--key', required=True, help='key file path')
     parser.add_argument('-r', '--root', required=True, help='root file path')
     parser.add_argument('-e', '--ep', required=True, help='endpoint')
-    parser.add_argument('-u', '--uuid', default='00000000-e132-1001-b000-001c4de2af03', help='uuid')
+    parser.add_argument('-u', '--uuid', default=devuuid, help='uuid')
 
     args = parser.parse_args()
 
